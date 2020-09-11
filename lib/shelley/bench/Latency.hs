e2bb0cd99b27781112bc04a305e3dcc242a8a003
Johannes Lund <johannes.lund@iohk.io>
2020-09-11 12:23:35 +0200

    Remove wallet and node integration logs from stdout
    And allow logging to a dedicated log directory by programatically
    modifying the CM.Configurations.

--- lib/shelley/bench/Latency.hs
+++ lib/shelley/bench/Latency.hs
@@ -374,6 +374,7 @@ withShelleyServer tracers action = do
                 Error
                 []
                 dir
+                Nothing
                 onByron
                 (afterFork dir)
                 (onClusterStart act dir)
